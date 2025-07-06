e664477ed87f485b4383d4ce30f18fd6002b5414
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-07-06 18:37:49 +0300

    add authentication app

---
+++ authentication/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Role(models.TextChoices):
+    ADMIN = "admin"
+    USER = "user"
+    TECHNICIAN = "technician"
+
+
+# Create your models here.
+class User(AbstractUser):
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.USER)
+    image = models.ImageField(
+        upload_to="users/profile_images/",
+        default="users/profile_images/default.webp",
+        null=True,
+        blank=True,
+    )
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.username
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.username
