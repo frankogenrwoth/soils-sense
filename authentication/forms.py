bdb777944337185bcd5665bb1022589c3cf69c63
ASJ234 <arikojoel1@gmail.com>
2025-07-08 17:04:18 +0300

    refactor password reset

--- authentication/forms.py
+++ authentication/forms.py
@@ -71,7 +71,7 @@ class SignupForm(forms.ModelForm):
 class PasswordResetForm(forms.Form):
     email = forms.EmailField(
         max_length=254,
-        widget=forms.EmailInput(attrs={'placeholder': 'Email'}),
+        widget=forms.EmailInput(attrs={'placeholder': 'Email', "class": "block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"}),
     )
     
     
