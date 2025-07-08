72dbdd7fc88eebc8fb86e8cdb0ce5d2c45512ceb
garsonj <garangayeljohn@gmail.com>
2025-07-08 11:50:28 +0300

    setting up landing page urls in the soil sense project

--- soilsense/urls.py
+++ soilsense/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("accounts/", include("authentication.urls")),
+    # Include the landing page URLs
+    path("", include("apps.landing_page.urls")),
+
 ]
