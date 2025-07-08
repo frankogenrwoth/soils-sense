e1ccd2f1d234cc6fd89680948ac1740bf740acb6
garsonj <garangayeljohn@gmail.com>
2025-07-08 17:20:01 +0300

    working on landing page app and making changes to soilsense project

--- soilsense/settings.py
+++ soilsense/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",  # Jazzmin for admin interface customization
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
