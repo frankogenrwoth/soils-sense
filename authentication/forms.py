47c6edfe08b3adacd3b638bebb9b0704bdbafc41
ASJ234 <arikojoel1@gmail.com>
2025-07-08 09:17:21 +0300

    completed the signup form, added the signupView using formView for user registration, and then added the signup url

--- authentication/forms.py
+++ authentication/forms.py
@@ -25,9 +25,28 @@ class LoginForm(forms.Form):
         return cleaned_data
     
 class SignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), label='Password')
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}), label='Confirm Password')
+
     class Meta:
         model = User
-        
+        fields = ['username', 'email', 'role', 'password', 'confirm_password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError('Passwords do not match')
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+    
 class PasswordResetForm(forms.Form):
     email = forms.EmailField(
         max_length=254,
--- authentication/views.py
+++ authentication/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import login, logout, authenticate
 from .forms import LoginForm
 from django.urls import reverse
 from django.shortcuts import redirect
+from django.views.generic.edit import FormView
+from .forms import SignupForm
 
 from .models import Role, User
 
@@ -44,3 +46,12 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect('authentication:login')
+
+class SignupView(FormView):
+    template_name = 'authentication/signup.html'
+    form_class = SignupForm
+    success_url = '/authentication/login/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
