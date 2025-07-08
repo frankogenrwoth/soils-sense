60ee3a3978ccc9700630d36139d16604aefc088a
ASJ234 <arikojoel1@gmail.com>
2025-07-08 11:05:23 +0300

    updated the user model to user Django's UserManager which has important functions eg normalize_email

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class Role(models.TextChoices):
@@ -19,7 +19,7 @@ class User(AbstractUser):
     )
     phone_number = models.CharField(max_length=15, null=True, blank=True)
 
-    objects = models.Manager()
+    objects = UserManager()
 
     def __str__(self):
         return self.username
