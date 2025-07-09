b6c59863b907d3c5e2c169b97313638296672e90
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-07-09 12:05:07 +0300

    add the administrator namespace

--- apps/administrator/urls.py
+++ apps/administrator/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from apps.administrator.views import UserManagementView
 
+app_name = "administrator"
+
 urlpatterns = [
     path("users/", UserManagementView.as_view(), name="users"),
 ]
--- apps/administrator/views.py
+++ apps/administrator/views.py
@@ -7,3 +7,7 @@ logger = logging.getLogger(__name__)
 
 class UserManagementView(View):
     template_name = "administrator/user_management.html"
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context=context)
